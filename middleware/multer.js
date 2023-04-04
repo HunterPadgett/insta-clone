3788739278b7bac0ba75e9dc52abee8859be0343
Hunter Padgett <gizmokabanjo@gmail.com>
2023-04-03 20:01:41 -0400

    img upload size change

--- middleware/multer.js
+++ middleware/multer.js
@@ -4,11 +4,13 @@ const path = require("path");
 module.exports = multer({
  storage: multer.diskStorage({}),
  fileFilter: (req, file, cb) => {
-  let ext = path.extname(file.originalname);
-  if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
+  const filetypes = /jpeg|jpg|png/;
+  const mimetype = filetypes.test(file.mimetype);
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  if (mimetype && extname) {
+   cb(null, true);
+  } else {
    cb(new Error("File type is not supported"), false);
-   return;
   }
-  cb(null, true);
  },
 });
